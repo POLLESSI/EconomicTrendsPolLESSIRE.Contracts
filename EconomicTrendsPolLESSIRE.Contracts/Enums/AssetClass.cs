797d4e53ec4a6b7a4d0f58ffa61fa7717e9e3638
POLLESSI <Pol PC@DESKTOP-K759EDQ>
2026-09-07 23:53:28 +0200

    Version 07 09 2026 Déclaration des premières enums

---
+++ EconomicTrendsPolLESSIRE.Contracts/Enums/AssetClass.cs
@@ -0,0 +1,13 @@
+﻿namespace EconomicTrendsPolLESSIRE.Contracts.Enums
+{
+    public enum AssetClass : byte
+    {
+        Stock = 1,
+        Etf = 2,
+        Commodity = 3,
+        Forex = 4,
+        Crypto = 5,
+        Index = 6,
+        Future = 7
+    }
+}
---
+++ EconomicTrendsPolLESSIRE.Contracts/Enums/CandleInterval.cs
@@ -0,0 +1,15 @@
+﻿namespace EconomicTrendsPolLESSIRE.Contracts.Enums
+{
+    public enum CandleInterval : short
+    {
+        OneSecond = 1,
+
+        OneMinute = 60,
+        FiveMinutes = 300,
+        FifteenMinutes = 900,
+
+        OneHour = 3600,
+
+        OneDay = 8640
+    }
+}
